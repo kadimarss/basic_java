7f4d1fac962ba4b5fd2557495610eb09cf5c4284
Kädi <kadi.marss@khk.ee>
2022-09-05 09:29:32 +0300

    Week 1 - Ex5

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Tere, Maailm!");
+        String text = "Java text";
+        int wholeNumber = 123;
+        Boolean isTrue = true;
+        System.out.println("texttext" + text);
+    }
+}
