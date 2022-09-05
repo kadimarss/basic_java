eb44d1e33f65928c126450f5268002d696ea60fc
Kädi <kadi.marss@khk.ee>
2022-09-05 20:58:47 +0300

    Week 1 - Ex5&Ex14

---
+++ week1/Ex14.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Ex14 {
+    public static void main(String[] args) {
+        int number = -4;
+
+        if (number >= 0) {
+            System.out.println("Your number is positive!");
+        } else {
+            System.out.println("Your number is negative!");
+        }
+
+    }
+}
