55082a27ec2cfe37ebe6ec412e6859a4b6bad579
Kädi <kadi.marss@khk.ee>
2022-09-09 09:15:19 +0300

    Week 4 - Ex72

---
+++ Week4/Ex72.java
@@ -0,0 +1,21 @@
+package Week4;
+
+import java.net.SocketOption;
+
+public class Ex72 {
+    public static void main(String[] args) {
+
+        Account mattAccount = new Account("Matt's account", 1000);
+        Account myAccount = new Account("My account", 0);
+        System.out.println(mattAccount);
+        System.out.println(myAccount);
+
+        mattAccount.withdrawal(100.0);
+        System.out.println(mattAccount);
+
+        myAccount.deposit(100.0);
+        System.out.println(myAccount);
+
+
+    }
+}
