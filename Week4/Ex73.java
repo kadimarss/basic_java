7496915795b45f3f2e2da3237bda5ee98456d19e
Kädi <kadi.marss@khk.ee>
2022-09-09 10:33:39 +0300

    Week 4 - Ex73

--- Week4/Ex73.java
+++ Week4/Ex73.java
@@ -4,5 +4,6 @@ public class Ex73 {
     public static void main(String[] args) {
         Product banana = new Product("Banana", 1.1, 13);
         banana.printProduct();
+        System.out.println(banana);
     }
 }
