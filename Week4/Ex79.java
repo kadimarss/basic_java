308d5c6dcc487862f9bda78cc645a2e0be50f02a
Kädi <kadi.marss@khk.ee>
2022-09-14 22:07:54 +0300

    Week 4 - Ex79

--- Week4/Ex79.java
+++ Week4/Ex79.java
@@ -1,13 +1,30 @@
 package Week4;
+
+import java.util.Scanner;
+
 public class Ex79 {
     public static void main(String[] args) {
-        NumberStatistics stats = new NumberStatistics();
-        stats.addNumber(3);
-        stats.addNumber(5);
-        stats.addNumber(1);
-        stats.addNumber(2);
-        System.out.println("Amount: " + stats.amountOfNumbers());
-        System.out.println("sum: " + stats.sum());
-        System.out.println("average: " + stats.average());
+        NumberStatistics all = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
+
+        System.out.println("Type number: ");
+        Scanner reader = new Scanner(System.in);
+        int number;
+        while (true) {
+            number = Integer.parseInt(reader.nextLine());
+            if (number == -1) {
+                break;
+            }
+            all.addNumber(number);
+            if(number % 2 == 0){
+                even.addNumber(number);
+            } else {
+                odd.addNumber(number);
+            }
+        }
+        System.out.println("sum: " + all.sum());
+        System.out.println("sum of even: " + even.sum());
+        System.out.println("sum of odd: " + odd.sum());
     }
 }
