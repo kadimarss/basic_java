73153172b12ce3ef7093fdf3733914d2d7c86e8c
Kädi <kadi.marss@khk.ee>
2022-09-16 17:33:25 +0300

    Week 8 - Ex12-12.2

---
+++ Week8/Storehouse.java
@@ -0,0 +1,43 @@
+package Week8;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storehouse {
+    private Map<String, Integer> products;
+    private Map<String, Integer> stocks;
+
+    public Storehouse() {
+        products = new HashMap<String, Integer>();
+        stocks = new HashMap<String, Integer>();
+    }
+
+    public void addProduct(String product, int price, int stock){
+        this.products.put(product, price);
+        this.stocks.put(product, stock);
+    }
+
+    public int price(String product){
+        @@ -21,4 +24,23 @@ public int price(String product){
+            return -99;
+        }
+    }
+
+    public int stock(String product){
+        if(this.stocks.containsKey(product)){
+            return this.stocks.get(product);
+        } else {
+            return 0;
+        }
+    }
+
+    public boolean take(String product){
+        if(this.stocks.containsKey(product)){
+            if(this.stocks.get(product) > 0){
+                this.stocks.put(product, this.stocks.get(product) - 1);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
