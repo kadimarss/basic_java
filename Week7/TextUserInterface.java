82a0d57889b3382fb730d82deb6a5af9e3252333
Kädi <kadi.marss@khk.ee>
2022-09-16 14:16:11 +0300

    Week 7 - Ex7 - 7.5

--- Week7/TextUserInterface.java
+++ Week7/TextUserInterface.java
@@ -1,19 +1,22 @@
 package Week7;
-
 import java.util.Scanner;
-
 public class TextUserInterface {
     private Dictionary dictionary;
     private Scanner reader;
     public TextUserInterface(Scanner reader, Dictionary dictionary) {
         this.dictionary = dictionary;
         this.reader = reader;
+        this.dictionary.add("add", "adds a word pair to the dictionary");
+        this.dictionary.add("translate", "asks a word and prints its translation");
         this.dictionary.add("quit", "quit the text user interface");
     }
 
     public void start(){
         System.out.println("Statement: ");
         System.out.println("quit - " + this.dictionary.translate("quit"));
+        for (String statement: this.dictionary.translationList()) {
+            System.out.println(statement);
+        }
         while(true){
             System.out.println("Statement: ");
             String input = reader.nextLine();
@@ -21,10 +24,19 @@ public class TextUserInterface {
                 System.out.println("Unknown statement");
             } else {
                 if(input.equals("quit")){
-                    System.out.println("Cheers!");
-                    break;
-                }
-            }
-        }
-    }
-}
+                    if(input.equals("add")){
+                        System.out.println("In Finnish:");
+                        String word = reader.nextLine();
+                        System.out.println("Translation:");
+                        String translation = reader.nextLine();
+                        this.dictionary.add(word, translation);
+                    } if(input.equals("translate")){
+                        System.out.println("Give a word:");
+                        String word = reader.nextLine();
+                        System.out.println("Translation:");
+                        System.out.println(this.dictionary.translate(word));
+                    }
+                    else if(input.equals("quit")){
+                        System.out.println("Cheers!");
+                        break;
+                    }
