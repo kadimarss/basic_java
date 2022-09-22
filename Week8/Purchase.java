96eb1f882e344bc581665f4bc5e9db908953b5e0
Kädi <kadi.marss@khk.ee>
2022-09-22 10:00:49 +0300

    Week8-Ex 12

--- Week8/Purchase.java
+++ Week8/Purchase.java
@@ -1,26 +1,45 @@
 package Week8;
-
 public class Purchase {
     private String product;
     private int amount;
     private int unitPrice;
-
     public Purchase(String product, int amount, int unitPrice) {
         this.product = product;
         this.amount = amount;
         this.unitPrice = unitPrice;
     }
 
+    public String getProduct() {
+        return product;
+    }
+
     public int price(){
         return this.unitPrice * this.amount;
     }
+    @@ -23,4 +27,26 @@ public void increaseAmount(){
+        public String toString() {
+            return this.product + ": " +this.amount;
+        }
 
-    public void increaseAmount(){
-        this.amount++;
-    }
+        public boolean equals(Object object) {
+            if (object == null) {
+                return false;
+            }
 
-    @Override
-    public String toString() {
-        return this.product + ": " +this.amount;
+            if (getClass() != object.getClass()) {
+                return false;
+            }
+
+            Purchase compared = (Purchase) object;
+
+            if (!this.product.equals(compared.product)) {
+                return false;
+            }
+
+            if (this.price() != compared.price()) {
+                return false;
+            }
+
+            return true;
+        }
     }
-}
